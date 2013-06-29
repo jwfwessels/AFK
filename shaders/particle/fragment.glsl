64398853b17f42a131ea0d78842fee2d23afed9f
Daniel Smith <jellymann@gmail.com>
2013-06-30 01:30:17 +0200

    Made particles circles.

--- shaders/particle/fragment.glsl
+++ shaders/particle/fragment.glsl
@@ -6,10 +6,12 @@ varying float ip;
 
 void main()
 {
-    /*float u = gl_TexCoord[0].x;
-    float v = 1.0 - gl_TexCoord[0].y;
+    float u = gl_TexCoord[0].x-0.5;
+    float v = 1.0 - gl_TexCoord[0].y-0.5;
 
-    gl_FragColor = texture2D(tex, vec2(u,v))*ip;*/
+    /*gl_FragColor = texture2D(tex, vec2(u,v))*ip;*/
+
+    if (u*u + v*v > 0.25) discard;
 
     gl_FragColor = vec4(ip,0,ip,1.0);
 }
