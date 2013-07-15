883bdb0eaf6160ffbd987f7fadb63d79b42f157a
Daniel Smith <jellymann@gmail.com>
2013-07-15 19:08:58 +0200

    Added simple shadows.

---
+++ shaders/simpleshadow/fragment.glsl
@@ -0,0 +1,20 @@
+#version 120
+
+//uniform sampler2D tex;
+
+uniform vec3 colour;
+
+varying float ip;
+
+void main()
+{
+    float u = gl_TexCoord[0].x-0.5;
+    float v = 1.0 - gl_TexCoord[0].y-0.5;
+
+    /*gl_FragColor = texture2D(tex, vec2(u,v))*ip;*/
+
+    if (u*u + v*v > 0.25) discard;
+
+    gl_FragColor = vec4(colour,1.0);
+}
+
