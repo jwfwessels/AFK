38056c9e92fb0c4e8cc455850ad7e761b7a0bc4a
Jessica Maclaurin <maclaurin.jess@gmail.com>
2013-08-17 12:50:26 +0200

    added new panel classes

---
+++ src/afk/frontend/swing/MatchSettingsPanel.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package afk.frontend.swing;
+
+/**
+ *
+ * @author Jessica
+ */
+public class MatchSettingsPanel {
+    
+}
