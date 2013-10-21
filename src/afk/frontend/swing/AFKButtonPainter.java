fc21eaf38972e1a99474fb0b74010091c12292a4
Jessica Maclaurin <maclaurin.jess@gmail.com>
2013-10-21 21:34:58 +0200

    added style for pressed state of button

--- src/afk/frontend/swing/AFKButtonPainter.java
+++ src/afk/frontend/swing/AFKButtonPainter.java
@@ -24,23 +24,73 @@ public class AFKButtonPainter extends SynthPainter
     @Override
     public void paintButtonBackground(SynthContext context, Graphics g, int x, int y, int w, int h)
     {
-        Graphics2D g2 = (Graphics2D) g;
-        Color start = UIManager.getColor("Button.first");
-        Color end = UIManager.getColor("Button.second");
-        GradientPaint grPaint = new GradientPaint((float) (x + w / 2), (float) y, start, (float) w / 2, (float) h, end);
-        g2.setPaint(grPaint);
-        g2.fillRect(x, y, w, h);
-        g2.setPaint(null);
+        System.out.println("COMPONENT STATE: " + context.getComponentState());
+        boolean pressed = false;
+        int state = context.getComponentState();
+        if(state >= 1024)
+        {
+            // DEFAULT
+            state -= 1024;
+        }
+        if(state >= 512)
+        {
+            // SELECTED
+            state -= 512;
+        }
+        if(state >= 256)
+        {
+            // FOCUSED
+            state -= 256;
+        }
+        if(state >= 8)
+        {
+            // DISABLED
+            state -= 8;
+        }
+        if(state >= 4)
+        {
+            // PRESSED
+            pressed = true;
+            state -= 4;
+        }
+        if(state >= 2)
+        {
+            // MOUSE_OVER
+            state -= 2;
+        }
+        if(state >= 1)
+        {
+            // ENABLED
+            state -= 2;
+        }
+        
+        if(pressed)
+        {
+            Graphics2D g2 = (Graphics2D) g;
+            Color start = UIManager.getColor("Button.third");
+            Color end = UIManager.getColor("Button.first");
+            GradientPaint grPaint = new GradientPaint((float) (x + w / 2), (float) y, start, (float) w / 2, (float) h, end);
+            g2.setPaint(grPaint);
+            g2.fillRect(x, y, w, h);
+            g2.setPaint(null);
+        }
+        else
+        {
+            Graphics2D g2 = (Graphics2D) g;
+            Color start = UIManager.getColor("Button.first");
+            Color end = UIManager.getColor("Button.second");
+            GradientPaint grPaint = new GradientPaint((float) (x + w / 2), (float) y, start, (float) w / 2, (float) h, end);
+            g2.setPaint(grPaint);
+            g2.fillRect(x, y, w, h);
+            g2.setPaint(null);
+        }
+        
+
     }
     
     @Override
     public void paintButtonBorder(SynthContext context, Graphics g, int x, int y, int w, int h)
-    {
-        System.out.println("SYNTH X:" + x);
-        System.out.println("SYNTH Y" + y);
-        System.out.println("SYNTH W" + w);
-        System.out.println("SYNTH H" + h);
-        
+    {   
         Graphics2D g2 = (Graphics2D) g;
         
         Paint borderPaint;
@@ -52,11 +102,10 @@ public class AFKButtonPainter extends SynthPainter
         g2.setStroke(borderStroke);
         g2.setPaint(borderPaint);
         g2.drawRect(x, y, w-1, h-1);
-        /*Graphics2D g2 = (Graphics2D) g;
-        Color temp = UIManager.getColor("Panel.first");
-        g2.setPaint(temp);
-        g2.setColor(temp);
-        g2.fillRect(x, y, w, h);
-        g2.setPaint(null);*/
+    }
+    
+    private void paintPressed(SynthContext context, Graphics g, int x, int y, int w, int h)
+    {
+        
     }
 }
