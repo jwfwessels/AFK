32eecafa94d734de7a0bc71e8e5a3f9f03f89e7e
Jessica Maclaurin <maclaurin.jess@gmail.com>
2013-10-23 16:51:24 +0200

    added function signitures for styling scroll bar

--- src/afk/frontend/swing/AFKScrollBarPainter.java
+++ src/afk/frontend/swing/AFKScrollBarPainter.java
@@ -4,11 +4,73 @@
  */
 package afk.frontend.swing;
 
+import java.awt.Graphics;
+import javax.swing.plaf.synth.SynthContext;
+import javax.swing.plaf.synth.SynthPainter;
+
 /**
  *
  * @author Jessica
  */
-public class AFKScrollBarPainter 
+public class AFKScrollBarPainter extends SynthPainter
 {
+    @Override
+    public void paintScrollBarBackground(SynthContext context, Graphics g, int x, int y, int w, int h)
+    {
+        
+    }
     
+    @Override
+    public void paintScrollBarBackground(SynthContext context, Graphics g, int x, int y, int w, int h, int orientation)
+    {
+        
+    }
+    
+    @Override
+    public void paintScrollBarBorder(SynthContext context, Graphics g, int x, int y, int w, int h)
+    {
+        
+    }
+    
+    @Override
+    public void paintScrollBarBorder(SynthContext context, Graphics g, int x, int y, int w, int h, int orientation)
+    {
+        
+    }
+    
+    @Override
+    public void paintScrollBarThumbBackground(SynthContext context, Graphics g, int x, int y, int w, int h, int orientation)
+    {
+        
+    }
+    
+    @Override
+    public void paintScrollBarThumbBorder(SynthContext context, Graphics g, int x, int y, int w, int h, int orientation)
+    {
+        
+    }
+    
+    @Override
+    public void paintScrollBarTrackBackground(SynthContext context, Graphics g, int x, int y, int w, int h)
+    {
+        
+    }
+    
+    @Override
+    public void paintScrollBarTrackBackground(SynthContext context, Graphics g, int x, int y, int w, int h, int orientation)
+    {
+        
+    }
+    
+    @Override
+    public void paintScrollBarTrackBorder(SynthContext context, Graphics g, int x, int y, int w, int h)
+    {
+        
+    }
+    
+    @Override
+    public void paintScrollBarTrackBorder(SynthContext context, Graphics g, int x, int y, int w, int h, int orientation)
+    {
+        
+    }
 }
