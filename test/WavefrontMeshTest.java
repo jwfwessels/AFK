529e4e4828a524d80565d9ab5266acdeaa0a6c92
daniel <daniel@iris>
2013-10-28 20:22:38 +0200

    Licensed source code under the MIT license.

--- test/WavefrontMeshTest.java
+++ test/WavefrontMeshTest.java
@@ -1,8 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 import afk.gfx.athens.WavefrontMesh;
 import org.junit.Test;
 import static org.junit.Assert.*;
